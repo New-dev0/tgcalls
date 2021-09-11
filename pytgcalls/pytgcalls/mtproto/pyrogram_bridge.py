55ee4ea3a80ffd865c196fe3a05f7c144f34669c
Devesh Pal <newdev0@outlook.com>
2021-09-11 21:06:34 +0530

    Update pyrogram_bridge.py

--- pytgcalls/pytgcalls/mtproto/pyrogram_bridge.py
+++ pytgcalls/pytgcalls/mtproto/pyrogram_bridge.py
@@ -106,14 +106,14 @@ class PyrogramBridge(MTProtoBridgeBase):
         return in_group_call
 
     async def get_my_volume(self):
-        if not self.full_chat or not self.full_chat.call or not self.my_peer:
+        if not self.full_chat or not self.full_chat.call or not self.my_peer or not self.my_ssrc:
             return
 
         response = await self.client.send(
             functions.phone.GetGroupParticipants(
                 call=self.full_chat.call,
                 ids=[self.my_peer],
-                sources=[],
+                sources=[int_ssrc(self.my_ssrc)],
                 offset="",
                 limit=1,
             )
